597f0e5ec24b906dc2e8ccbb4f00583145f5284d
Robson <robsonribeirorra@gmail.com>
2023-05-23 18:37:14 -0300

    Initial Commit

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+const { User } = require('../../src/app/models');
+
+describe('', () => {
+    it('should', () => {
+
+        expect().toBe();
+    });
+});
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+const routes = require('express').Router();
+const { User } = require('./app/models');
+
+
+
+
+module.exports = routes;
